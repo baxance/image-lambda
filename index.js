c5898c6ec111ecf79ae33e1022e69264951ebf08
Barrett <baxance@protonmail.com>
2021-11-09 18:37:59 -0800

    nothing

---
+++ index.js
@@ -0,0 +1,17 @@
+const AWS = require('aws-sdk');
+let s3 = new AWS.S3();
+require('dotenv').config();
+
+exports.handler = async (event) => {
+    
+    console.log(s3);
+    let bucket = event["Records"][0]["s3"]["bucket"];
+    let object = event["Records"][0]["s3"]["object"];
+    console.log(bucket, object);
+    
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
